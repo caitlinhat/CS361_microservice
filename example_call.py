a4be9e187f5c258f573cb1494e0d657d44226989
Caitlin Hathaway <103151440+caitlinhat@users.noreply.github.com>
2023-11-20 14:17:53 -0600

    Add files via upload

---
+++ example_call.py
@@ -0,0 +1,15 @@
+import time
+
+
+def get_input():
+    with open("rng_output.txt", 'w') as file:
+        file.write("request")
+    file.close()
+
+    with open("rng_output.txt", 'r') as file:
+        time.sleep(5)
+        poke_num = file.readline()
+    file.close()
+
+
+get_input()
---
+++ prng-service.py
@@ -0,0 +1,25 @@
+import random
+import time
+
+
+def prng_service():
+    """
+    Opens a .txt file and checks if it contains "run".
+    If so, generate a random integer from 1 to 1000.
+    Function then overwrites the file with the generated integer.
+    """
+    time.sleep(1)
+    # open prng service
+    with open('rng_output.txt', 'r') as file:
+        # if file says "run", run the generator then erase run
+        if "request" in file:
+            with open('rng_output.txt', 'w') as open_file:
+                num = str(random.randint(1, 1000))
+                open_file.write(num)
+
+    # close prng service
+    file.close()
+
+
+while True:
+    prng_service()
